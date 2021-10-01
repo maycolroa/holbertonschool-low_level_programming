98fe8633d3375c9df48b881c9de8ca99f0b426f1
maycolroa <lehallo@hotmail.com>
2021-10-01 11:31:31 -0500

    0x01-variables_if_else_while 0.00 01/10/21

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+* main - proyec n positive or negative
+* 
+* Return: Always 0 
+*/
+int main(void)
+{
+int n;
+if(n = 0)
+{
+printf("is zero %d\n");
+}
+else if (n < 0)
+{
+printf("is negative%d\n", n);
+}
+else (n > 0)
+{
+printf("is positive%d\n", n);
+}
+return (0);
+}
