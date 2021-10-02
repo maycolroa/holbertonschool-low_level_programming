d1cae1e9c4ce8ac4eb1cbe8b0256e6baaf17b9cc
maycolroa <lehallo@hotmail.com>
2021-10-02 16:32:03 -0500

    0x01. C - Variables, if, else, while  0.04 02/10/21

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'q')
+if (i != 'e')
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
