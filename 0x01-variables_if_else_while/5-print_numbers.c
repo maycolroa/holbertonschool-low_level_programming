d35781f44612ca73d614777906b05e89ae7f4416
maycolroa <lehallo@hotmail.com>
2021-10-02 17:03:22 -0500

    0x01. C - Variables, if, else, while  0.05 02/10/21

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - project printing numbers 0-9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
