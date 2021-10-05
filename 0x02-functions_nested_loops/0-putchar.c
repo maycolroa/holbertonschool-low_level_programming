d813c7b3d3a17c4f16b9651f8758643d39da8026
maycolroa <lehallo@hotmail.com>
2021-10-05 11:15:21 -0500

    0x02. C - Functions, nested loops  0.00 04/10/21

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;       
+char a[] = "_putchar\n";
+for (i = 0; i <= 8; i++)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
+return (0);
+}
