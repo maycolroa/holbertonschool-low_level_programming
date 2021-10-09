b0560afd36d6fae07f84dea2fdfde0745028a762
maycolroa <lehallo@hotmail.com>
2021-10-08 20:41:35 -0500

    0x04. C - More functions, more nested loops  0.01 08/10/21

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _isdigit - Write a function that checks for
+  * @c: character
+  *
+  * Return: 1 o 0
+  */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
