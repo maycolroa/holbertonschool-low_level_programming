4ebbaf5f29e891251f4014e76040f457ccfe7b1e
maycolroa <lehallo@hotmail.com>
2021-10-11 14:15:39 -0500

    0x05. C - Pointers, arrays and strings  0.01 11/10/21

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98 - print
+ *@n: array
+ *
+ *Return: 0.
+ */
+void swap_int(int *a, int *b)
+{
+int i;
+i = *a;
+*a = *b;
+*b = i;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 #include <unistd.h>
 #include <stdio.h>
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
