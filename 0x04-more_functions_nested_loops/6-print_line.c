c43cb03d114755393423155e7a5bd5b3156cc737
maycolroa <lehallo@hotmail.com>
2021-10-10 22:53:51 -0500

    0x04. C - More functions, more nested loops  0.06 10/10/21

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_line - print_
+  *@n: int
+  *@i: int 2
+  * Return: 0
+  */
+void print_line(int n)
+{
+int i;
+if (n < 1)
+{
+putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+putchar('_');
+}
+putchar('\n');
+}
+}
