f714290d2494ef81cf08107b75769c4f52cf5a9d
maycolroa <lehallo@hotmail.com>
2021-10-12 22:29:27 -0500

    0x05. C - Pointers, arrays and strings  0.07 12/10/21

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *puts_half - print half
+ *@str: pointer
+ *Return: 0.
+ */
+void puts_half(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+i++;
+for (i /= 2; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
