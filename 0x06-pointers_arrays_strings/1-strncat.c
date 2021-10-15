e2169a7ab1b1f581aa01cbd0fa23f0f198368a07
maycolroa <lehallo@hotmail.com>
2021-10-14 22:11:33 -0500

    0x06. C - More pointers, arrays and strings  0.01 14/10/21

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strcat - print array holberton 
+ * @dest: Array 1
+ * @src: array 2
+ *Return: 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j, k;
+for (i = 0; dest[i] != '\0'; i++);
+for (k = 0; src[k] != '\0'; k++);
+if (n > k)
+{
+n = k;
+}
+for (j = 0; j < n;)
+{
+dest[i + j] = src[j];
+j++;
+}
+return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 #include <unistd.h>
 #include <stdio.h>
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 #endif
