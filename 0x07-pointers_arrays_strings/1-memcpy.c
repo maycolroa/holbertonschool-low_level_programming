3344f7c52dc65aa0bcaf7288f3bb7016e661d1d6
maycolroa <lehallo@hotmail.com>
2021-10-19 12:18:15 -0500

    0x07. C - Even more pointers, arrays and strings  0.01 19/10/21

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_memcpy - print array
+ * @dest: Array 1
+ * @src: array 2
+ * @n: int
+ *Return: s.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
