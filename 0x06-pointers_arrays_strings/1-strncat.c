02f993478d08f18ba0b5227d43996b9719ef05c4
maycolroa <lehallo@hotmail.com>
2021-10-19 11:48:46 -0500

    0x06. C - More pointers, arrays and strings  0.01 19/10/21

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 int i, j;
 i = 0;
 j = 0;
-while(dest[j] != '\0')
+while (dest[j] != '\0')
 {
 j++;
-}		;
+}
 while (src[i] != '\0' && i < n)
 {
 dest[j + i] = src[i];
