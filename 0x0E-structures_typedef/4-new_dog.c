45ebdf499558a0e96b87d29377e060bf28cc448d
maycolroa <lehallo@hotmail.com>
2021-11-02 11:45:16 -0500

    0x0E. C - Structures, typedef  0.03 02/11/21

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+  * new_dog - new
+  * @name: struct name
+  * @age: struct age
+  * @owner: struct owner
+  * Return: 0
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{int i, j, k;
+
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+
+	for (j = 0; name[j]; j++)
+		;
+	for (k = 0; owner[k]; k++)
+		;
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = malloc(sizeof(char) * (j + 1));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->owner = malloc(sizeof(char) * (k + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= j; i++)
+		new_dog->name[i] = name[i];
+	new_dog->age = age;
+
+	for (i = 0; i <= k; i++)
+		new_dog->owner[i] = owner[i];
+
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,5 @@ typedef struct dog
 #define FUNCTIONS_i
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
