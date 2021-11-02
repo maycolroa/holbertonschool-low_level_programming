360c83f2ba60a4a257fb02d334225bad019e6e0c
maycolroa <lehallo@hotmail.com>
2021-11-02 15:34:21 -0500

    0x0A. C - argc, argv  0.05 02/11/21

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * main - program that adds positive numbers.
+ * @argc: arg count.
+ * @argv: arg vector.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int exchange;
+	int currency;
+
+	currency = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	exchange = atoi(argv[1]);
+	for  ( exchange = atoi(argv[1]); exchange > 0; currency++)
+	{
+		if (exchange >= 25)
+			exchange = exchange - 25;
+		else if (exchange >= 10)
+			exchange = exchange - 10;
+		else if (exchange >= 5)
+			exchange = exchange - 5;
+		else if (exchange >= 2)
+			exchange = exchange - 2;
+		else
+			exchange = exchange - 1;
+	}
+	printf("%i\n", currency);
+	return (0);
+}
