e68d92995832b4bf76bc3c8413d450447ea924bf
maycolroa <lehallo@hotmail.com>
2021-11-01 21:34:41 -0600

    0x0E. C - Structures, typedef  0.00 01/11/21

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
