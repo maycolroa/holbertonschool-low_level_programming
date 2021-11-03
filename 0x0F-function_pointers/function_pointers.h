43e1cbbb8c811f5dae2351a9b1de3bad0fbba683
maycolroa <lehallo@hotmail.com>
2021-11-03 12:53:19 -0500

    0x0F. C - Punteros de función  0.00 03/11/21

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
-#include "stdlib.h"
+#include <stdlib.h>
 #include <stdio.h>
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
