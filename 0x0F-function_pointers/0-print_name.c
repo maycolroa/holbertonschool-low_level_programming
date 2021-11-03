444a70767639afc931fd92c8fdf9e10a3d0a6ae4
maycolroa <lehallo@hotmail.com>
2021-11-03 10:55:50 -0500

    0x0F. C - Punteros de función  0.00 03/11/21

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+  * print_name - prints
+  * @name: string
+  * @f: function pointer
+  */
+void print_name(char *name, void (*f)(char *))
+{
+        if (name != NULL)
+        if (*name != '\0')
+        if (f != NULL)
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+#include "stdlib.h"
+#include <stdio.h>
+void print_name(char *name, void (*f)(char *));
+#endif
