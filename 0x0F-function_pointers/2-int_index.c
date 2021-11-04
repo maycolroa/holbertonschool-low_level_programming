23eecc6c74b0a7c20b12e80d77e411cc1294fd4b
maycolroa <lehallo@hotmail.com>
2021-11-04 15:19:02 -0500

    0x0F. C - Punteros de función  0.02 04/11/21

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,19 +1,19 @@
 #include "function_pointers.h"
 /**
-  * int_index - function that searches for an integer.
+  * int_index - function that searches for an integer
   * @array: pointer 1
   * @size: array
   * @cmp: pointer 2
+  * Return: -1
   */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-i = -1;
 if (size <= 0)
 if (array != NULL)
 if (cmp != NULL)
 return (-1);
-while (++i < size)
+for (i = -1; i < size; i++)
 {
 if (cmp(array[i]))
 return (i);
