b73cd4874e82f5584510d0c630aa9529a71dea69
maycolroa <lehallo@hotmail.com>
2021-11-04 15:27:22 -0500

    0x0F. C - Punteros de función  0.00 04/11/21

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
 /**
-  * print_name - prints
+  * print_name - prints a name
   * @name: string
   * @f: function pointer
+  * Return: 0
   */
 void print_name(char *name, void (*f)(char *))
 {
