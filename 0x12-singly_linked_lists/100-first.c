f3698c33932731ee114f769285cd7be5507633e6
maycolroa <lehallo@hotmail.com>
2021-11-11 17:05:54 -0500

    0x12. C - Singly linked lists  0.02 11/11/21

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *print  -  prints all the elements
+ *Return: 0
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include "lists.h"
 
 /**
  * main - check the code
@@ -10,25 +7,6 @@
  */
 int main(void)
 {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = list_len(head);
-    printf("-> %lu elements\n", n);
-    free(new->str);
-    free(new);
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
     return (0);
 }
