482acc4183178642fb2b4d7c047c69040e9c69e5
maycolroa <lehallo@hotmail.com>
2021-11-17 11:01:29 -0500

    0x13. C - More singly linked lists  0.02 17/11/21

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+  * add_nodeint -  adds a new node
+  * @n: int
+  * @head: position list
+  * Return: the address of the new element, or NULL if it failed
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
