97876a83c5fcf6ee17d3304192494ae9ce9998a5
maycolroa <lehallo@hotmail.com>
2021-11-16 23:04:11 -0500

    0x13. C - More singly linked lists  0.01 16/11/21

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,14 @@
 #include "lists.h"
 /**
-  * print_listint - prints list
+  * listint_len - print list
   * @h: pointer
   * Return: nodes
   */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 size_t i;
 
 for (i = 0; h; i++)
-{
-printf("%d\n", h->n);
 h = h->next;
-}
 return (i);
 }
