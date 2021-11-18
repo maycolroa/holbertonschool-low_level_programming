bbc425fa679b94d7a56d06b2513513ab4ea2a1f7
maycolroa <lehallo@hotmail.com>
2021-11-18 12:29:59 -0500

    0x14. C - Bit manipulation  0.00 18/11/21

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,8 +10,8 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base_then = 0;
-	unsigned int exp = 1;
+	unsigned int i = 0;
+	unsigned int j = 1;
 	int oper = 0;
 
 	if (b == NULL)
@@ -19,15 +19,15 @@ unsigned int binary_to_uint(const char *b)
 
 while (b[oper] != '\0')
 {
-        oper ++;
+oper++;
 }
 for (oper -= 1; oper >= 0; oper--)
 	{
 		if (b[oper] != '0' && b[oper] != '1')
 			return (0);
-		base_then += (b[oper] - '0') * exp;
-		exp *= 2;
+		i += (b[oper] - '0') * j;
+		j *= 2;
 	}
 
-	return (base_then);
+	return (i);
 }
