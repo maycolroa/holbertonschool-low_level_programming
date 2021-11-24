d55fa026f83065bc3fd1b9863e7373f150bb01f0
maycolroa <lehallo@hotmail.com>
2021-11-24 08:53:57 -0500

    0x14. C - Bit manipulation  0.00 24/11/21

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+  * read_textfile - reads a text file and prints it to posxi
+  * @filename: name file
+  * @letters: number characters
+  * Return: 0
+  **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
