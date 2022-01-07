8b39138105862e833d1a17aef3eab7959418b69c
maycolroa <lehallo@hotmail.com>
2022-01-06 23:27:38 -0500

    0x17. C - Doubly linked lists  0.02 06/01/22

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -20,6 +20,8 @@ typedef struct dlistint_s
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
+#include "lists.h"
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
