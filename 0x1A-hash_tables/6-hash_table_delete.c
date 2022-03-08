009a7912f04b7f1c8231c8a6546a83db5ddaf6a9
maycolroa <lehallo@hotmail.com>
2022-03-07 23:43:23 -0500

    0x1A. C - Hash tables  0.06  07/03/2022

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table.
+ * @ht: hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int j;
+	hash_node_t *next;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+	for (j = 0; j < ht->size; j++)
+	{
+		while (ht->array[j] != NULL)
+		{
+			next = ht->array[j]->next;
+			free(ht->array[j]->key);
+			free(ht->array[j]->value);
+			free(ht->array[j]);
+			ht->array[j] = next;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
