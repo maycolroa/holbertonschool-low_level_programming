c07c4a48005cb18197eba11829caf72889abe39e
maycolroa <lehallo@hotmail.com>
2022-03-07 22:46:46 -0500

    0x1A. C - Hash tables  0.04  07/03/2022

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,12 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int j;
 	hash_node_t *aux;
 
-if (ht == NULL)
-if (ht->array == NULL)
-if (ht->size == 0)
-if (key == NULL)
-if (strlen(key) == 0)
-return (NULL);
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	j = key_index((const unsigned char *)key, ht->size);
 	aux = ht->array[j];
 	while (aux != NULL)
