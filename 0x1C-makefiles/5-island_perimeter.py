fe33438b34b87d5b8cc5830d4b8fd5c446f9bb72
maycolroa <lehallo@hotmail.com>
2022-03-21 23:36:25 -0500

    0x1C. C - Makefiles 21/03/22 0.05

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create a function def island_perimeter"""
+
+
+def island_perimeter(grid):
+    """returns in grid"""
+    perimeter = 0
+    for j in range(0, len(grid), 1):
+        for k in range(0, len(grid[0]), 1):
+            if grid[j][k] == 1:
+                perimeter = perimeter + 4
+                if k - 1 >= 0 and grid[j][k - 1] == 1:
+                    perimeter = perimeter - 2
+                if j - 1 >= 0 and grid[j - 1][k] == 1:
+                    perimeter = perimeter - 2
+    return (perimeter)
